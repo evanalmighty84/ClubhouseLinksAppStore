88ec0d9eab7f1ef50920dcbb3e6096f1383edf4a
Evan Ligon <evan@Evans-MBP.lan>
2026-07-08 16:32:23 -0500

    old signin apple button

--- ClubhouseResident/Sources/Views/SignupView.swift
+++ ClubhouseResident/Sources/Views/SignupView.swift
@@ -62,9 +62,6 @@ struct SignupView: View {
                 .padding()
             }
             .scrollDismissesKeyboard(.interactively)
-            .onTapGesture {
-                hideKeyboard()
-            }
         }
         .navigationTitle("Sign Up")
         .navigationBarTitleDisplayMode(.inline)
